70446687126a966bbd2b02c157315b34ff5e98cf
shuvro baset <md.abdul.baset75@gmail.com>
2021-10-25 21:20:48 +0600

    form ready

--- crud-app/src/pages/AddUser/AddUser.js
+++ crud-app/src/pages/AddUser/AddUser.js
@@ -1,14 +1,38 @@
+import Button from '@restart/ui/esm/Button';
 import React from 'react';
-import { Container, Row } from 'react-bootstrap';
+import { Col, Container, Form, Row } from 'react-bootstrap';
 
 const AddUser = () => {
 
 
 
     return (
-        <Container>
-            <Row>
-                <h2>Add Your New User Info.</h2>
+        <Container className="my-5">
+                <h2 className="text-center">Add Your New User Info.</h2>
+
+            <Row className='d-flex justify-content-center align-items-center my-5'>
+                <Col md={6}>
+                    <Form>
+                        <Form.Group className="mb-3" >
+                            <Form.Control type="text" placeholder="First Name" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Control type="text" placeholder="Last Name" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" >
+                            <Form.Control type="email" placeholder="Email" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Control type="text" placeholder="Address" />
+                        </Form.Group>
+                        
+                        <button className="btn btn-primary" type="submit">
+                            Submit
+                        </button>
+                    </Form>
+                </Col>
             </Row>
         </Container>
     );
